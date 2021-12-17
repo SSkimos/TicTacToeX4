e22caee8c23b2bdc50ab43c6d8c5e0a1242e74b7
Chereshnev <chereshnev.n.s@edu.mirea.ru>
2021-12-17 18:36:54 +0300

    interim version
    improved view controller

--- TicTacToe/Field.swift
+++ TicTacToe/Field.swift
@@ -2,8 +2,8 @@ import Foundation
 
 struct Field {
     //двумерный массив для хранения класса клетка
-    var cells = (repeating: Array(repeating: 0, count: 6), count: 6)
-    //var cells = Array(repeating: Array(repeating: 0, count: 9), count: 9)
+    var cells = Array(repeating: Array(repeating: Cell(), count: 6), count: 6)
+    
     var symbol: Int = 1 //счетчик очередности хода
     var scoreX: Int = 0  //счетчики для хранения очков игроков Х и О
     var scoreO: Int = 0
--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -4,12 +4,17 @@
 //
 //  Created by Никита Черешнев on 03.12.2021.
 //
-//TODO:удалить из памяти кнопок инфу о старой коллекции
+//TODO: удалить из памяти кнопок инфу о старой коллекции
+//TODO: понять как сделать нормально код
+//TODO: придумать как сделать двумерную матрицу экземпляров класса cell
+//TODO: придумать как связать клетку (2ную матрицу) с массивом кнопок (1мерная матрица)
+
 import UIKit
 
 class ViewController: UIViewController {
     
     var ticTacToeGame = TicTacToeGame() //экземпляр класса TicTacToeGame
+    var flag: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,69 +27,65 @@ class ViewController: UIViewController {
     // лейбл счета игроков
     @IBOutlet weak var playersScore: UILabel!
     
-//    private func updateViewFromModel() {
-//        for index in .indices {
-//            let button = buttonCollection[index]
-//            let card = game.cards[index]
-//            if card.isFacedUp {
-//                button.setTitle(emojiIdentifier(for: card), for: .normal)
-//                button.backgroundColor = UIColor.white
-//            } else {
-//                button.setTitle("", for: .normal)
-//                //button.backgroundColor = UIColor.systemBlue
-//                button.backgroundColor = card.isMatched ? UIColor.white : UIColor.systemBlue
-//            }
-//        }
-//    }
+    private func getImageName(x: Int, y: Int) -> String {
+        return "\(String(ticTacToeGame.field.cells[x][y].symbol))\(String(ticTacToeGame.field.cells[x][y].vertical))\(String(ticTacToeGame.field.cells[x][y].horizontal))\(String(ticTacToeGame.field.cells[x][y].mainDiagonal))\(String(ticTacToeGame.field.cells[x][y].sideDiagonal))"
+    }
     
-    private func updateViewFromModel(var x: Int, var y: Int) {
-        ticTacToeGame.newSymbol(x: x, y: y)
+    private func updateViewFromModel(x: Int, y: Int) {
+        for index in 0...35 {
+            if ticTacToeGame.field.cells[index/6][index%6].symbol == 1 || ticTacToeGame.field.cells[index/6][index%6].symbol == -1 {
+                if var image = UIImage(named: getImageName(x: index/6, y: index%6)) {
+                    buttonFieldArray[index].setImage(image, for: .normal)
+                }
+            }
+        }
     }
     
     //функция, которая отвечает за нажатие кнопок на игровом поле №1
     @IBAction func buttonFirstFieldAction(_ sender: UIButton) {
-        if let buttonIndex = buttonFieldArray.firstIndex(of: sender) {
-            //buttonFieldArray[buttonIndex].backgroundColor? = UIColor.gray
-            //buttonFieldArray[buttonIndex].setTitle("\((buttonIndex)/6),\((buttonIndex)%6)", for: .normal)
-            //updateViewFromModel()
-//            if var image = UIImage(named: "-1falsetruefalsefalse") {
-//                buttonFieldArray[buttonIndex].setImage(image, for: .normal)
-//            }
-            updateViewFromModel(var: buttonIndex/6, var: buttonIndex%6)
+        if flag <= 9 {
+            if let buttonIndex = buttonFieldArray.firstIndex(of: sender) {
+                if ticTacToeGame.newSymbol(x: buttonIndex/6, y: buttonIndex%6) != false {
+                    updateViewFromModel(x: buttonIndex/6, y: buttonIndex%6)
+                }
+            }
+            flag+=1
         }
     }
     
     //№2
     @IBAction func buttonSecondFieldAction(_ sender: UIButton) {
-        if let buttonIndex = buttonFieldArray.firstIndex(of: sender) {
-            //buttonFieldArray[buttonIndex].backgroundColor? = UIColor.gray
-            //buttonFieldArray[buttonIndex].setTitle("\((buttonIndex)/6),\((buttonIndex)%6)", for: .normal)
-            //updateViewFromModel()
-//            if var image = UIImage(named: "-1falsetruefalsetrue") {
-//                buttonFieldArray[buttonIndex].setImage(image, for: .normal)
-//            }
+        if flag >= 10 || flag <= 17 {
+            if let buttonIndex = buttonFieldArray.firstIndex(of: sender) {
+                if ticTacToeGame.newSymbol(x: buttonIndex/6, y: buttonIndex%6) != false {
+                    updateViewFromModel(x: buttonIndex/6, y: buttonIndex%6)
+                }
+            }
+            flag+=1
         }
     }
     
     //№3
     @IBAction func buttonThirdFieldAction(_ sender: UIButton) {
-        if let buttonIndex = buttonFieldArray.firstIndex(of: sender) {
-            //buttonFieldArray[buttonIndex].backgroundColor? = UIColor.gray
-            //buttonFieldArray[buttonIndex].setTitle("\((buttonIndex)/6),\((buttonIndex)%6)", for: .normal)
-            //updateViewFromModel()
-//            if var image = UIImage(named: "-1falsetruetruefalse") {
-//                buttonFieldArray[buttonIndex].setImage(image, for: .normal)
-//            }
+        if flag >= 18 || flag <= 26 {
+            if let buttonIndex = buttonFieldArray.firstIndex(of: sender) {
+                if ticTacToeGame.newSymbol(x: buttonIndex/6, y: buttonIndex%6) != false {
+                    updateViewFromModel(x: buttonIndex/6, y: buttonIndex%6)
+                }
+            }
+            flag+=1
         }
     }
     
     //№4
     @IBAction func buttonFourthFieldAction(_ sender: UIButton) {
-        if let buttonIndex = buttonFieldArray.firstIndex(of: sender) {
-            //buttonFieldArray[buttonIndex].setTitle("\((buttonIndex)/6),\((buttonIndex)%6)", for: .normal)
-//            if var image = UIImage(named: "-1falsetruetruetrue") {
-//                buttonFieldArray[buttonIndex].setImage(image, for: .normal)
-//            }
+        if flag >= 27 || flag <= 35 {
+            if let buttonIndex = buttonFieldArray.firstIndex(of: sender) {
+                if ticTacToeGame.newSymbol(x: buttonIndex/6, y: buttonIndex%6) != false {
+                    updateViewFromModel(x: buttonIndex/6, y: buttonIndex%6)
+                }
+            }
+            flag+=1
         }
     }
 }
